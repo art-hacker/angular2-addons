e301ed0c456d28b59f24acb1a22843d67cde54ab
hck <art.hacker@ya.ru>
2017-06-06 11:05:38 +0300

    Init

---
+++ focus/focus.ts
@@ -0,0 +1,1 @@
+export {AutoFocusModule} from "./src/AutoFocusModule";
---
+++ focus/src/AutoFocusModule.ts
@@ -0,0 +1,13 @@
+import {NgModule} from "@angular/core";
+
+import {AutoFocusDirective} from "./Directive/AutoFocusDirective";
+
+@NgModule({
+    declarations: [
+        AutoFocusDirective
+    ],
+    exports: [
+        AutoFocusDirective
+    ]
+})
+export class AutoFocusModule {}
